e57245e3b0674edf0f439e5cf18e7e2baf8c1893
Ken Ellorando <kenellorando@gmail.com>
2024-06-01 17:01:18 -0500

    Add hex conversion

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
 fn main() {
-    let binary: u32 = 0b1101;
+    let hex: u32 = 0xa000;
     let mut decimal: u32 = 0;
     let mut power: u32 = 0;
 
-    for digit in format!("{:b}", binary).chars().rev() {
-        decimal += digit.to_digit(2).unwrap() * 2_u32.pow(power);
+    for digit in format!("{:x}", hex).chars().rev() {
+        decimal += digit.to_digit(16).unwrap() * 16_u32.pow(power);
         power += 1;
     }
 
     println!("{}", decimal);
+
+
+    // let binary: u32 = 0b0011000000111001;
+    // let mut decimal: u32 = 0;
+    // let mut power: u32 = 0;
+
+    // for digit in format!("{:b}", binary).chars().rev() {
+    //     decimal += digit.to_digit(2).unwrap() * 2_u32.pow(power);
+    //     power += 1;
+    // }
+
+    // println!("{}", decimal);
 }
